ed5cb3c5e56aca0684785bcbe09ff72511ac2b06
Bohdan00735 <bodya00735@gmail.com>
2021-02-07 10:47:12 +0200

    Initial commit

---
+++ app/src/main/java/com/mobiledevelopmentworks/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.mobiledevelopmentworks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
+        actionBar?.hide();
+        supportActionBar?.hide()
+
+        val navController = findNavController(R.id.container)
+        navView.setupWithNavController(navController)
+    }
+}
