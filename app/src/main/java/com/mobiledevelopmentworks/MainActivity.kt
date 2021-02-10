3bb99d5d525cc99539102e64639d3b1e656245d6
Bohdan00735 <bodya00735@gmail.com>
2021-02-10 17:04:52 +0200

    graphic views added

--- app/src/main/java/com/mobiledevelopmentworks/MainActivity.kt
+++ app/src/main/java/com/mobiledevelopmentworks/MainActivity.kt
@@ -1,25 +1,21 @@
 package com.mobiledevelopmentworks
 
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
 import androidx.annotation.RequiresApi
-import androidx.navigation.NavController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.aghajari.graphview.AXGraphFormula
+import com.aghajari.graphview.AXGraphOptions
+import com.aghajari.graphview.AXGraphView
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.mobiledevelopmentworks.lab1.coordinatesData.CoordinateBM
-import com.mobiledevelopmentworks.lab1.coordinatesData.DirectionConverter
+import kotlin.math.pow
+
 
 @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class MainActivity : AppCompatActivity() {
-    private lateinit var coordinate:CoordinateBM
+
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,41 +24,12 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
         actionBar?.hide();
         supportActionBar?.hide()
-
         val navController = findNavController(R.id.container)
         navView.setupWithNavController(navController)
+
+
     }
 
-    fun saveCoordinate(view: View){
-        val degree = findViewById<EditText>(R.id.degree_input).text.toString().toInt()
-        val minutes = findViewById<EditText>(R.id.minutes_input).text.toString().toUInt()
-        val seconds = findViewById<EditText>(R.id.seconds_import).text.toString().toUInt()
-        val direction = findViewById<EditText>(R.id.dirrection_input).text.toString()
-        coordinate = CoordinateBM(DirectionConverter().getDirection(direction)!!, degree, minutes,seconds)
-    }
 
-    fun getCoordinate(view: View){
-        findViewById<TextView>(R.id.result_view).append(coordinate.getCoordinate())
-    }
 
-    fun getDecimalCoordinate(view: View){
-        findViewById<TextView>(R.id.result_view).append(coordinate.getDecimalCoordinate())
-    }
-
-    fun calculateMiddleOfTwoCoordinates(view: View){
-        var degree = findViewById<EditText>(R.id.degree_input1).text.toString().toInt()
-        var minutes =findViewById<EditText>(R.id.minutes_input1).text.toString().toUInt()
-        var seconds = findViewById<EditText>(R.id.seconds_import1).text.toString().toUInt()
-        var direction = findViewById<EditText>(R.id.dirrection_input1).text.toString()
-        val coordinate1 = CoordinateBM(DirectionConverter().getDirection(direction)!!, degree, minutes,seconds)
-        degree = findViewById<EditText>(R.id.degree_input2).text.toString().toInt()
-        minutes = findViewById<EditText>(R.id.minutes_input2).text.toString().toUInt()
-        seconds = findViewById<EditText>(R.id.seconds_import2).text.toString().toUInt()
-        direction = findViewById<EditText>(R.id.dirrection_input2).text.toString()
-        val coordinate2 = CoordinateBM(DirectionConverter().getDirection(direction)!!, degree, minutes,seconds)
-
-        findViewById<TextView>(R.id.result_view).append(
-                CoordinateBM().calculateMediumCoordinateFromInput(coordinate1,coordinate2)?.
-                getCoordinate().toString())
-    }
 }
--- app/src/main/java/com/mobiledevelopmentworks/MoreFragment.kt
+++ app/src/main/java/com/mobiledevelopmentworks/MoreFragment.kt
@@ -1,14 +1,24 @@
 package com.mobiledevelopmentworks
 
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import com.mobiledevelopmentworks.R
-import com.mobiledevelopmentworks.lab1.coordinatesData.CoordinateBM
-import com.mobiledevelopmentworks.lab1.coordinatesData.DirectionConverter
+import android.widget.ToggleButton
+import androidx.fragment.app.Fragment
+import com.aghajari.graphview.AXGraphFormula
+import com.aghajari.graphview.AXGraphOptions
+import com.aghajari.graphview.AXGraphView
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.cos
 
 /**
  * A simple [Fragment] subclass.
@@ -16,19 +26,78 @@ import com.mobiledevelopmentworks.lab1.coordinatesData.DirectionConverter
  * create an instance of this fragment.
  */
 class MoreFragment : Fragment() {
+    lateinit var graphView: AXGraphView
+    lateinit var pieView:PieChart
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        activity?.findViewById<EditText>(R.id.degree_input)
-        return inflater.inflate(R.layout.fragment_more, container, false)
+        val root = inflater.inflate(R.layout.fragment_more, container, false)
+        graphView = root.findViewById<AXGraphView>(R.id.graph_view)
+        pieView = root.findViewById(R.id.pie_view)
+        setGraph()
+        setPieView()
+        root.findViewById<ToggleButton>(R.id.change_graphic_button).setOnCheckedChangeListener{
+                _, isChecked ->
+            if (isChecked) {
+                pieView.visibility = View.VISIBLE
+                graphView.visibility = View.INVISIBLE
+            } else {
+                graphView.visibility = View.VISIBLE
+                pieView.visibility = View.INVISIBLE
+            }
+        }
+
+        return root
     }
 
-    fun createSmth(view: View){}
+    private fun setGraph(){
+        val options = AXGraphOptions(activity);
+
+        options.scrollEnabled = true;
+        options.drawGridXLines = false
+        options.drawGridYLines = false
+        options.drawXText = false
+        options.drawYText = false
+        options.xDividerInterval = PI.toFloat()/4
+        options.maxZoom = 6f
+        options.minZoom = 0.5f
+        graphView.graphOptions = options;
+
+        graphView.addFormula(object : AXGraphFormula() {
+            override fun function(x: Float): Float {
+                return cos(x)
+            }
+
+            override fun isInDomain(x: Float): Boolean {
+                return abs(x) <= PI
+            }
+        })
+    }
+
+    private fun setPieView(){
+        val  entries = ArrayList<PieEntry>()
+        entries.add(PieEntry(45f))
+        entries.add(PieEntry(5f))
+        entries.add(PieEntry(25f))
+        entries.add(PieEntry(25f))
+        val colorsList = ArrayList<Int>()
+        colorsList.add(Color.CYAN)
+        colorsList.add(Color.MAGENTA)
+        colorsList.add(Color.YELLOW)
+        colorsList.add(Color.GRAY)
+        val pieDataSet = PieDataSet(entries, "Pie chart")
+        pieDataSet.colors = colorsList
+        pieDataSet.sliceSpace = 1f;
+        pieDataSet.valueTextColor = Color.DKGRAY
+        pieDataSet.valueTextSize = 10f;
+        pieView.data = PieData(pieDataSet)
+    }
+
 }
